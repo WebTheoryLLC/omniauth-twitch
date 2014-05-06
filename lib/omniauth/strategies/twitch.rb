ce441439b107d62f43caf353bd2053f44f5420ed
Jonathan Gertig <jcgertig@gmail.com>
2014-05-06 03:14:08 -0500

    full gem

--- lib/omniauth/strategies/twitch.rb
+++ lib/omniauth/strategies/twitch.rb
@@ -3,6 +3,7 @@ require 'omniauth-oauth2'
 module OmniAuth
   module Strategies
     class Twitch < OmniAuth::Strategies::OAuth2
+      DEFAULT_SCOPE = 'user_read'
 
       option :name, "twitch"
 
@@ -11,6 +12,13 @@ module OmniAuth
         authorize_url: "/kraken/oauth2/authorize",
         token_url: '/kraken/oauth2/token'
       }
+
+      option :access_token_options, {
+        header_format: 'OAuth %s',
+        param_name: 'access_token'
+      }
+
+      option :authorize_options, [:scope]
 
       uid{ raw_info['_id'] }
 
@@ -23,6 +31,36 @@ module OmniAuth
           logo: raw_info['logo'],
           type: raw_info['type']
         }
+      end
+
+      extra do
+        {
+          raw_info: raw_info
+        }
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get('/kraken/user.json').parsed
+      end
+
+      def build_access_token
+        super.tap do |token|
+          token.options.merge!(access_token_options)
+        end
+      end
+
+      def access_token_options
+        options.access_token_options.inject({}) { |h,(k,v)| h[k.to_sym] = v; h }
+      end
+
+      def authorize_params
+        super.tap do |params|
+          options[:authorize_options].each do |k|
+            params[k] = request.params[k.to_s] unless [nil, ''].include?(request.params[k.to_s])
+          end
+
+          params[:scope] = params[:scope] || DEFAULT_SCOPE
+        end
       end
     end
   end
