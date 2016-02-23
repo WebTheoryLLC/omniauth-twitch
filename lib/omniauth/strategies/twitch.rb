7ede3cba00b73a4a3d93c212a233ced80e350c18
johnwahba <john.a.wahba@gmail.com>
2016-02-22 16:57:52 -0800

    Update twitch.rb

--- lib/omniauth/strategies/twitch.rb
+++ lib/omniauth/strategies/twitch.rb
@@ -58,7 +58,7 @@ module OmniAuth
           options[:authorize_options].each do |k|
             params[k] = request.params[k.to_s] unless [nil, ''].include?(request.params[k.to_s])
           end
-          unless !options[:redirect_uri].nil?
+          if !options[:redirect_uri].nil?
             params[:redirect_uri] = options[:redirect_uri]
           end
           params[:scope] = params[:scope] || DEFAULT_SCOPE
