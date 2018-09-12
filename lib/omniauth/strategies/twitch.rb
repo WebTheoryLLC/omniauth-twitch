34164f14a2b76090567d644575b3c61d40820005
Jonathan Gertig <jcgertig@gmail.com>
2018-09-12 08:10:33 -0500

    spacing fix

--- lib/omniauth/strategies/twitch.rb
+++ lib/omniauth/strategies/twitch.rb
@@ -21,7 +21,7 @@ module OmniAuth
       option :authorize_options, [:scope]
 
       def request_phase
-        redirect client.auth_code.authorize_url({:redirect_uri => callback_url}.merge(authorize_params)).gsub(/%2[b,B]/,'+')
+        redirect client.auth_code.authorize_url({ redirect_uri: callback_url }.merge(authorize_params)).gsub(/%2[b,B]/,'+')
       end
 
       credentials do
